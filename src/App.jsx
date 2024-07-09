62f53a540e3970c9cecfe19b430762d7ab00f127
Harshal Bodhe <harshalbodhe22@gmail.com>
2024-07-09 12:48:02 +0530

    new add

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+function App() {
+  return (
+    <div className="w-full h-screen bg-zinc-900 text-white">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
