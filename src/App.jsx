e793057372e6b9e76fbfb6f5f87974604a7b9830
unknown <shaileshpanghate29816@gmail.com>
2024-07-12 21:12:18 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,27 @@ import Navbar from "./components/Navbar";
 import Landing from "./components/Landing";
 import Marquee from "./components/Marquee";
 import Footer from "./components/Footer";
+import About from './components/About'
+import Eyes from './components/Eyes'
+import Fetured from './components/Fetured'
+import Cards from './components/Cards'
+import Ready from './components/Ready'
+import LocomotiveScroll from 'locomotive-scroll';
 
 function App() {
+
+  const locomotiveScroll = new LocomotiveScroll();
+
   return (
     <div className="w-full min-h-screen bg-zinc-900 text-white">
       <Navbar />
       <Landing />
       <Marquee />
+      <About />
+      <Eyes />
+      <Fetured />
+      <Cards />
+      <Ready />
       <Footer />
     </div>
   );
