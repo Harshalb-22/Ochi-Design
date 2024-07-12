9333b1b473ed61b85aa58ba4cfc1f8d52df447e7
Harshal Bodhe <harshalbodhe22@gmail.com>
2024-07-12 21:01:35 +0530

    adding animation

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ import Navbar from "./components/Navbar";
 import Landing from "./components/Landing";
 import Marquee from "./components/Marquee";
 import Footer from "./components/Footer";
+import LocomotiveScroll from "locomotive-scroll";
 
 function App() {
+  // eslint-disable-next-line no-unused-vars
+  const locomotiveScroll = new LocomotiveScroll();
   return (
     <div className="w-full min-h-screen bg-zinc-900 text-white">
       <Navbar />
